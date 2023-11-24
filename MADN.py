04388f8b6459d9ac133c3b8137ad31da05d06db9
Baboon9 <tim_schuerheck@gmx.de>
2023-11-24 18:17:11 +0100

    complete overhaul

--- MADN.py
+++ MADN.py
@@ -1,72 +1,158 @@
+import unittest
+
+class GameMethodsTests(unittest.TestCase):
+
+        def test_arrayLengths(self):
+            self.assertTrue(len(game.figures) == game.player_count*4, "Figure Length Wrong!") 
+            self.assertTrue(len(game.board.houses) == game.player_count, "Houses Length not correct!" )
+            self.assertTrue(len(game.board.homes) == game.player_count, "Player Count not correct!" )
+            self.assertTrue(len(game.board.field) == game.player_count  *12, "Game Field length is not right!" )
+            self.assertTrue(len(game.figures) == game.player_count *4, "The amount of Figures is not correct!" )
 
 
-class MADNExtreme:
-    gamestates = ["initializing","running","ending","setup"]
-    currentState = gamestates[3]
-    def initialize(self):
-        print("Initializing Game")
-        self.board=Board()
-        self.board.generateBoard()
+class Game:
+    def __init__(self):
+        self.game_states = ["initializing","starting","running","stopping","ending"]
+        self.game_state = self.game_states[0]
+        self.player_count = int(input("How many players are there? Enter a number from 1-8: "))
+        self.board = Board(self.player_count)
+        
+        self.figures = []
+        self.players = []
+        
+        for i in range(0,self.player_count):
+            self.players.append(Player(str(input("Enter your name: ")),i+1))
 
-    def run():
-        pass
-    def end():
-        pass
-    def setup(self):
-        print("How many players? (max 8)")
-        self.playerCount=int(input())
-        print("What boardsize? (max 4)")
-        self.boardSize=int(input())
-        print("What difficulty? (0-10")
-        self.difficulty=int(input())
-        print("Game has been Setup with the parameters Playercount, Boardsize, and Difficulty")
-        if (not self.checkForGameSetupConstraints()):
-            print("You need to set reasonable parameters..")
-            self.setup()
+        for player in self.players:
+            for j in range (0,4):
+                self.figures.append(Figure(player.color))
 
-        self.currentState = self.gamestates[0]
+        self.board.generateBoard(self.player_count)
+        
+        
 
-    
-    def checkForGameSetupConstraints(self):
-        if self.playerCount > 8 or self.playerCount < 2:
-            return False
-        if self.boardSize > 4 or self.boardSize < 1:
-            return False
-        if self.difficulty > 10 or self.difficulty < 0:
-            return False
-        return True
+        self.computer_player_count = int(input("Against how many Computer players do you want to play? Enter a number between 0-4: "))
+        
+        self.start()
+
+
+    def start(self):
+        print("Game is starting..")
+        self.game_state = self.game_states[1]
+        self.board.setInFigures(self.player_count, self.figures)
+        self.game_state = self.game_states[2]
+
+        self.run()
+
+    def run(self):
+        print("Game is running..")
+        print("printing board..")
+        self.print()
+        #while self.game_state == self.game_states[2]:
+        #    pass
+
+    def print(self):
+        self.board.print()
+
 
 class Board:
-    def generateBoard(self):
-        self.houses = []
-        self.board = []
-        for player in range(game.playerCount):
-            self.houses.append(input("Please enter Playername: "))
+    def __init__(self, player_count):
+        self.player_count=player_count
+        self.houses=[]
+        self.homes=[]
+        self.field=[]
+
+
+    def print(self):
+        print()
+        for house in self.houses:
+            for spot in house:
+                spot.print()
+        print("\n^^^^^^\nhouse\n")
+        for home in self.homes:
+            for spot in home:
+                spot.print()
+        print("\n^^^^^^\nhome\n")
+        for field in self.field:
+            field.print()
+        print("\n^^^^^^\nfield\n")
+
+
+    def generateBoard(self, player_count):
+        for i in range(0,self.player_count):
+            self.houses.append([])  
+
+        for i in range(0,4):
+            for house in self.houses:
+                house.append(Spot(None))
+
+        for i in range(0,self.player_count):
+            self.homes.append([])
+
+        for i in range(0,4):
+            for home in self.homes:
+                home.append(Spot(None))
+
+        for i in range(0,12*self.player_count):
+            self.field.append(Spot(None) )
+    
+
+    def setInFigures(self,player_count,figures):
+        for i in range(0,player_count):
+            for j in range(0,4*player_count):
+                self.houses[i][int(j/player_count)].setInFigure(figures[j])
+
         for spot in self.houses:
-            for i in range(4 * (game.boardSize * 4 )):
-                self.board.append(0)
-        for i in range(game.playerCount):
-            self.board[i * 4 + game.boardSize * 4] = 1
+            print(spot)    
 
-    def printBoard(self):
-        for i in self.board:
-            print(i)
 
-class Network:
-    pass
+class Figure:
+    def __init__(self, color):
+        self.color = color
+    def getFigureOfColorAsInt(self,integer):
+        if self.color == integer:
+            return self
+
+class Spot:
+    def __init__(self, figure):
+        self.figureAtSpot = figure
+    def setInFigure(self, figure):
+        self.figureAtSpot = figure
+    def print(self):
+        if not self.figureAtSpot == None:
+            print(self.figureAtSpot.color,end="")
+        else:
+            print("O",end="")
+
 
 class Player:
-    pass
+    def __init__(self, name, color):
+        self.name=name
+        self.color=color
+
+class Computer:
+    def __init__(self):
+        pass
+
+class Home:
+    def __init__(self):
+        pass
+
+class House:
+    def __init__(self):
+        pass
+
+class Field:
+    def __init__(self):
+        pass
+
+class Network:
+    def __init__(self):
+        pass
 
 
+game = Game()
 
-game = MADNExtreme()
+test = GameMethodsTests()
 
-print("Available Game States:")
-print(game.gamestates)
-print("Current Game State:")
-print(game.currentState)
-game.setup()
-game.initialize()
-game.board.printBoard()
-
+unittest.main()
